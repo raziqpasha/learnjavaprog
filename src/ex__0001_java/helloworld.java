5adb571714033bd496dbdcb7caca1536fdc55c5b
raziqpasha <sjraziq@gmail.com>
2025-03-10 21:16:37 +0530

    modified helloworld.java code

--- src/ex__0001_java/helloworld.java
+++ src/ex__0001_java/helloworld.java
@@ -3,5 +3,6 @@ package ex__0001_java;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello bangaluru");
+        System.out.println("bye bangalurur");
     }
 }
